ebb1ebf83fb44bfa131c9c3350f6adb5a61db0ca
haohaoget <2287405159wh@gmail.com>
2026-04-12 16:45:32 +0800

    fix: 由于busybox无法提供ip monitor, 判断系统是否存在ip和sh功能，不存在回退inotifyd监测

--- box_service.sh
+++ box_service.sh
@@ -8,31 +8,39 @@
     if [ -f "/data/adb/box/scripts/start.sh" ]; then
         chmod 755 /data/adb/box/scripts/*
         /data/adb/box/scripts/start.sh
-
+        SCRIPTS_DIR="/data/adb/box/scripts"
 
         while [ ! -f /data/misc/net/rt_tables ]; do
             sleep 3
         done
-        # 清理可能残留的旧监听进程
-        pkill -f "ip monitor rule"
-        # 使用基于 Netlink 的内核路由规则监听
-        (
-            ip monitor rule | while read -r event; do
-                case "$event" in
-                    *"from all fwmark 0x0/0xffff iif lo lookup "*)
-                        
-                        # 防抖逻辑：如果 .inotify 没在运行，则触发执行并传入网络接口参数
-                        if ! pgrep -f "/data/adb/box/scripts/net.inotify" > /dev/null; then
-                            sh /data/adb/box/scripts/net.inotify w> /dev/null 2>&1 &
-                        fi
-                        if ! pgrep -f "/data/adb/box/scripts/ctr.inotify" > /dev/null; then
-                            sh /data/adb/box/scripts/ctr.inotify w> /dev/null 2>&1 &
-                        fi
-                        ;;
-                esac
-            done
-        ) > /dev/null 2>&1 &
-        # inotifyd ${SCRIPTS_DIR}/ctr.inotify /data/misc/net/rt_tables > /dev/null 2>&1 &
+
+        IP_BIN="/system/bin/ip"
+        SH_BIN="/system/bin/sh"
+        if [ -f "$IP_BIN" && -f "$SH_BIN"]; then
+            # 确保启动前清理可能残留的旧监听进程
+            pkill -f "$IP_BIN monitor rule"
+            # 替换为基于 Netlink 的内核路由规则监听
+            (
+            $IP_BIN monitor rule | while read -r event; do
+                    case "$event" in
+                        *"from all fwmark 0x0/0xffff iif lo lookup "*)
+                            # 防抖逻辑：如果 .inotify 没在运行，则触发执行并传入网络接口参数
+                            if ! pgrep -f "/data/adb/box/scripts/net.inotify" > /dev/null; then
+                                $SH_BIN /data/adb/box/scripts/net.inotify w> /dev/null 2>&1 &
+                            fi
+                            
+                            if ! pgrep -f "/data/adb/box/scripts/ctr.inotify" > /dev/null; then
+                                RUN_DIR="/data/adb/box/run" \
+                                $SH_BIN /data/adb/box/scripts/ctr.inotify w> /dev/null 2>&1 &
+                            fi
+                            ;;
+                    esac
+                done
+            ) > /dev/null 2>&1 &
+        else
+            inotifyd ${SCRIPTS_DIR}/net.inotify /data/misc/net/rt_tables > /dev/null 2>&1 &
+            inotifyd ${SCRIPTS_DIR}/ctr.inotify /data/misc/net/rt_tables > /dev/null 2>&1 &
+        fi
     else
         echo "未找到文件 '/data/adb/box/scripts/start.sh'"
     fi
